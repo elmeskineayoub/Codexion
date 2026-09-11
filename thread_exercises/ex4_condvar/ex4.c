214d47a1a9a8abf8ab9f464b1dceb60752e2544c
aelmeski <ayoubelmeskine4@gmail.com>
2026-09-11 07:12:20 +0100

    a

--- thread_exercises/ex4_condvar/ex4.c
+++ thread_exercises/ex4_condvar/ex4.c
@@ -19,6 +19,11 @@ void *producer(void *arg)
 	// print "producer: setting ready"
 	// signal the cond var
 	// unlock the mutex
+	pthread_mutex_lock(&lock);
+	ready = 1;
+	printf("producer: setting ready\n");
+	pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&lock);
 	return (NULL);
 }
 
@@ -31,6 +36,11 @@ void *consumer(void *arg)
 	//   (try `if` first on purpose, see hints)
 	// print "consumer: got it"
 	// unlock the mutex
+	pthread_mutex_lock(&lock);
+	if(!ready)
+		pthread_cond_wait(&cond, &lock);
+	printf("consumer: got it\n");
+	pthread_mutex_unlock(&lock);
 	return (NULL);
 }
 
@@ -38,6 +48,17 @@ int main(void)
 {
 	pthread_t p, c;
 
+	pthread_mutex_init(&lock, NULL);
+	pthread_cond_init(&cond, NULL);
+
+	pthread_create(&p, NULL, &producer, NULL);
+	pthread_create(&c, NULL, &consumer, NULL);
+
+	pthread_join(p, NULL);
+	pthread_join(c, NULL);
+
+	pthread_mutex_destroy(&lock);
+	pthread_cond_destroy(&cond);
 	// TODO: init mutex and cond var
 	// TODO: create producer and consumer threads
 	// TODO: join both
