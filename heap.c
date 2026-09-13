3f110b843b360653bba0d774d4ba29e8a2e875a8
Ayoub Elmeskine <aelmeski@e1r4p2.1337.ma>
2026-09-13 19:38:46 +0100

    a

--- heap.c
+++ heap.c
@@ -6,75 +6,81 @@
 /*   By: aelmeski <aelmeski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 11:40:22 by aelmeski          #+#    #+#             */
-/*   Updated: 2026/09/13 11:40:58 by aelmeski         ###   ########.fr       */
+/*   Updated: 2026/09/13 18:38:09 by aelmeski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-// heap_create(capacity, scheduler):
-//     heap = malloc(t_heap)
-//     heap->data = malloc(capacity * sizeof(t_request))
-//     heap->size = 0
-//     heap->capacity = capacity
-//     heap->scheduler = scheduler
-//     return heap
+int	heap_push(t_heap *heap, t_request req)
+{
+	int	i;
 
+	if (heap->size == heap->capacity)
+		return (1);
+	heap->data[heap->size] = req;
+	i = heap->size;
+	heap->size++;
+	sift_up(heap, i);
+	return (0);
+}
 
+void	sift_up(t_heap *heap, int i)
+{
+	int	parent;
 
+	while (i > 0)
+	{
+		parent = (i - 1) / 2;
+		if (cmp_request(&heap->data[i], &heap->data[parent], heap->scheduler))
+		{
+			swap(&heap->data[i], &heap->data[parent]);
+			i = parent;
+		}
+		else
+			break ;
+	}
+}
 
+void	sift_down(t_heap *heap, int i)
+{
+	int	left;
+	int	best;
+	int	right;
 
-// cmp_request(a, b, scheduler):
-//     # returns true if 'a' should come BEFORE 'b' (a has higher priority)
-//     if scheduler == FIFO:
-//         return a->arrival < b->arrival
-//     else:  # EDF
-//         if a->deadline != b->deadline:
-//             return a->deadline < b->deadline
-//         return a->arrival < b->arrival   # tie-break
+	while (1)
+	{
+		left = 2 * i + 1;
+		right = 2 * i + 2;
+		best = i;
+		if (left < heap->size && cmp_request(&heap->data[left],
+				&heap->data[best], heap->scheduler))
+			best = left;
+		if (right < heap->size && cmp_request(&heap->data[right],
+				&heap->data[best], heap->scheduler))
+			best = right;
+		if (best == i)
+			break ;
+		swap(&heap->data[i], &heap->data[best]);
+		i = best;
+	}
+}
 
-// heap_push(heap, req):
-//     if heap->size == heap->capacity:
-//         return error   # shouldn't happen, capacity = num_coders
-//     heap->data[heap->size] = req
-//     i = heap->size
-//     heap->size++
-//     sift_up(heap, i)
+int	heap_pop(t_heap *heap, t_request *out)
+{
+	if (heap->size == 0)
+		return (1);
+	*out = heap->data[0];
+	heap->size--;
+	heap->data[0] = heap->data[heap->size];
+	sift_down(heap, 0);
+	return (0);
+}
 
-// sift_up(heap, i):
-//     while i > 0:
-//         parent = (i - 1) / 2
-//         if cmp_request(&heap->data[i], &heap->data[parent], heap->scheduler):
-//             swap(heap->data[i], heap->data[parent])
-//             i = parent
-//         else:
-//             break
-
-// heap_pop(heap, out):
-//     if heap->size == 0:
-//         return error
-//     *out = heap->data[0]              # root = highest priority
-//     heap->size--
-//     heap->data[0] = heap->data[heap->size]   # move last element to root
-//     sift_down(heap, 0)
-//     return success
-
-// sift_down(heap, i):
-//     while true:
-//         left = 2*i + 1
-//         right = 2*i + 2
-//         best = i
-//         if left < heap->size and cmp_request(&heap->data[left], &heap->data[best], heap->scheduler):
-//             best = left
-//         if right < heap->size and cmp_request(&heap->data[right], &heap->data[best], heap->scheduler):
-//             best = right
-//         if best == i:
-//             break
-//         swap(heap->data[i], heap->data[best])
-//         i = best
-
-// heap_peek(heap, out):
-//     if heap->size == 0:
-//         return error
-//     *out = heap->data[0]
-//     return success
+int	heap_peek(t_heap *heap, t_request *out)
+{
+	if (heap->size == 0)
+		return (1);
+	*out = heap->data[0];
+	return (0);
+}
---
+++ heap_utils.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heap_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelmeski <aelmeski@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/13 18:28:52 by aelmeski          #+#    #+#             */
+/*   Updated: 2026/09/13 18:38:06 by aelmeski         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+void	swap(t_request *a, t_request *b)
+{
+	t_request	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	cmp_request(t_request *a, t_request *b, t_scheduler scheduler)
+{
+	if (scheduler == FIFO)
+	{
+		if (a->arrival != b->arrival)
+			return (a->arrival < b->arrival);
+		return (a->coder_id < b->coder_id);
+	}
+	if (a->deadline != b->deadline)
+		return (a->deadline < b->deadline);
+	if (a->arrival != b->arrival)
+		return (a->arrival < b->arrival);
+	return (a->coder_id < b->coder_id);
+}
+
+t_heap	*heap_create(int capacity, int scheduler)
+{
+	t_heap	*heap;
+
+	heap = malloc(sizeof(t_heap));
+	if (!heap)
+		return (NULL);
+	heap->data = malloc(capacity * sizeof(t_request));
+	if (!heap->data)
+	{
+		free(heap);
+		return (NULL);
+	}
+	heap->size = 0;
+	heap->capacity = capacity;
+	heap->scheduler = scheduler;
+	return (heap);
+}
+
+void	heap_destroy(t_heap *heap)
+{
+	if (!heap)
+		return ;
+	free(heap->data);
+	free(heap);
+}
